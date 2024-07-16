8fe6f4fee7aaa829c5c9cdca08d9df021d7130f7
Arina <arina.milyutina.0005@gmail.com>
2024-07-17 00:02:42 +0300

    created repository and service for working with entity author

---
+++ src/main/java/com/example/project/service/AuthorService.java
@@ -0,0 +1,11 @@
+package com.example.project.service;
+
+import com.example.project.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+    @Autowired
+    private AuthorRepository authorRepository;
+}
