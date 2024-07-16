cafda3346ee13e00244ab5bb99d16d08be761947
Arina <arina.milyutina.0005@gmail.com>
2024-07-17 01:02:16 +0300

    added method findByAuthorId

--- src/main/java/com/example/project/repository/BookRepository.java
+++ src/main/java/com/example/project/repository/BookRepository.java
@@ -3,6 +3,8 @@ package com.example.project.repository;
 import com.example.project.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book,Long> {
+import java.util.List;
 
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByAuthorId(Long authorId);
 }
