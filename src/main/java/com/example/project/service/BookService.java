8e1f33a648e8790f6e3ee3832f85f53c2e6a36f2
Arina <arina.milyutina.0005@gmail.com>
2024-07-17 00:50:35 +0300

    added method findBookById

--- src/main/java/com/example/project/service/BookService.java
+++ src/main/java/com/example/project/service/BookService.java
@@ -1,20 +1,33 @@
 package com.example.project.service;
 
 import com.example.project.entity.Book;
+import com.example.project.exception.NotFoundException;
 import com.example.project.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
-    private void createdBook(Book book){
+
+    private void createdBook(Book book) {
         bookRepository.save(book);
     }
-    private List<Book> findAll(){
+
+    private List<Book> findAll() {
         return bookRepository.findAll();
     }
+
+    private Optional<Book> findBookById(Long bookId) {
+        return bookRepository.findById(bookId);
+    }
+
+
+
+
 }
