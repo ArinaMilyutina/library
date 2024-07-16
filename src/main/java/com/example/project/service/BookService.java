f30fe433258600c6beac4e80268aa85c8a3fada8
Arina <arina.milyutina.0005@gmail.com>
2024-07-17 00:02:55 +0300

    created repository and service for working with entity book

---
+++ src/main/java/com/example/project/service/BookService.java
@@ -0,0 +1,11 @@
+package com.example.project.service;
+
+import com.example.project.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+}
