f4befe118ad6073006c4af521a74ce7b032bc84f
Arina <arina.milyutina.0005@gmail.com>
2024-07-17 01:12:06 +0300

    added method updateAuthor

--- src/main/java/com/example/project/service/AuthorService.java
+++ src/main/java/com/example/project/service/AuthorService.java
@@ -1,6 +1,8 @@
 package com.example.project.service;
 
 import com.example.project.entity.Author;
+import com.example.project.entity.Book;
+import com.example.project.exception.NotFoundException;
 import com.example.project.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,14 +14,30 @@ import java.util.Optional;
 public class AuthorService {
     @Autowired
     private AuthorRepository authorRepository;
-    private void createdAuthor(Author author){
+
+    private void createdAuthor(Author author) {
         authorRepository.save(author);
     }
-    private List<Author> findAll(){
+
+    private List<Author> findAll() {
         return authorRepository.findAll();
     }
-    private Optional<Author> findById(Long authorId){
+
+    private Optional<Author> findById(Long authorId) {
         return authorRepository.findById(authorId);
     }
 
+    private void updateAuthor(Long authorId, Author updateAuthor) {
+        Optional<Author> authorOptional = findById(authorId);
+        if (authorOptional.isPresent()) {
+            Author author = authorOptional.get();
+            author.setName(updateAuthor.getName());
+            author.setSurname(updateAuthor.getSurname());
+            author.setBirthDate(updateAuthor.getBirthDate());
+            author.setBooks(updateAuthor.getBooks());
+            createdAuthor(author);
+        } else {
+            throw new NotFoundException("Book not found with id: " + authorId);
+        }
+    }
 }
