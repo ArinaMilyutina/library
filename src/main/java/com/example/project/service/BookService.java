43dee54fc310ba2657c131f763ed00c9df5f3994
Arina <arina.milyutina.0005@gmail.com>
2024-07-17 00:16:42 +0300

    added methods(createdBook and findAll)

--- src/main/java/com/example/project/service/BookService.java
+++ src/main/java/com/example/project/service/BookService.java
@@ -1,11 +1,20 @@
 package com.example.project.service;
 
+import com.example.project.entity.Book;
 import com.example.project.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
+    private void createdBook(Book book){
+        bookRepository.save(book);
+    }
+    private List<Book> findAll(){
+        return bookRepository.findAll();
+    }
 }
