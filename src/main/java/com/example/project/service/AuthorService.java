6d45943a4a210366a117372511e3ae0a03cfdf27
Arina <arina.milyutina.0005@gmail.com>
2024-07-17 00:16:59 +0300

    added methods(createdAuthor and findAll)

--- src/main/java/com/example/project/service/AuthorService.java
+++ src/main/java/com/example/project/service/AuthorService.java
@@ -1,11 +1,20 @@
 package com.example.project.service;
 
+import com.example.project.entity.Author;
 import com.example.project.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthorService {
     @Autowired
     private AuthorRepository authorRepository;
+    private void createdAuthor(Author author){
+        authorRepository.save(author);
+    }
+    private List<Author> findAll(){
+        return authorRepository.findAll();
+    }
 }
