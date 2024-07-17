b540fe502bd0c28be20ecd9cd88ce86635d2cdf5
Arina <arina.milyutina.0005@gmail.com>
2024-07-17 20:08:51 +0300

    fixed method createdBook

--- src/main/java/com/example/project/service/BookService.java
+++ src/main/java/com/example/project/service/BookService.java
@@ -1,7 +1,10 @@
 package com.example.project.service;
 
+import com.example.project.dto.BookDto;
+import com.example.project.entity.Author;
 import com.example.project.entity.Book;
 import com.example.project.exception.NotFoundException;
+import com.example.project.repository.AuthorRepository;
 import com.example.project.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,35 +16,45 @@ import java.util.Optional;
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private AuthorRepository authorRepository;
 
-    private void createdBook(Book book) {
-        bookRepository.save(book);
+    public Book createdBook(BookDto bookDto) {
+        Author author = authorRepository.findById(bookDto.getAuthorId()).orElseThrow(() -> new RuntimeException("Author not found"));
+        Book book = Book.builder()
+                .title(bookDto.getTitle())
+                .genre(bookDto.getGenre())
+                .dateOfPublication(bookDto.getDateOfPublication())
+                .author(author)
+                .build();
+        return bookRepository.save(book);
+
     }
 
-    private List<Book> findAll() {
+    public List<Book> findAll() {
         return bookRepository.findAll();
     }
 
-    private Optional<Book> findBookById(Long bookId) {
+    public Optional<Book> findBookById(Long bookId) {
         return bookRepository.findById(bookId);
     }
 
 
-    private void updateBook(Long bookId, Book updateBook) {
-        Optional<Book> bookOptional = findBookById(bookId);
-        if (bookOptional.isPresent()) {
-            Book book = bookOptional.get();
-            book.setAuthor(updateBook.getAuthor());
-            book.setTitle(updateBook.getTitle());
-            book.setGenre(updateBook.getGenre());
-            book.setDateOfPublication(updateBook.getDateOfPublication());
-            createdBook(book);
-        } else {
-            throw new NotFoundException("Book not found with id: " + bookId);
-        }
-    }
+//    public void updateBook(Long bookId, Book updateBook) {
+//        Optional<Book> bookOptional = findBookById(bookId);
+//        if (bookOptional.isPresent()) {
+//            Book book = bookOptional.get();
+//            book.setAuthor(updateBook.getAuthor());
+//            book.setTitle(updateBook.getTitle());
+//            book.setGenre(updateBook.getGenre());
+//            book.setDateOfPublication(updateBook.getDateOfPublication());
+//            createdBook(book);
+//        } else {
+//            throw new NotFoundException("Book not found with id: " + bookId);
+//        }
+//    }
 
-    private void deleteBook(Long bookId) {
+    public void deleteBook(Long bookId) {
         Optional<Book> bookOptional = findBookById(bookId);
         if (bookOptional.isPresent()) {
             bookRepository.deleteById(bookId);
@@ -50,7 +63,7 @@ public class BookService {
         }
     }
 
-    private List<Book> findByAuthorId(Long authorId) {
+    public List<Book> findByAuthorId(Long authorId) {
         return bookRepository.findByAuthorId(authorId);
     }
 }
