4f7f75e22082eb556fc9e90021480a0db0c0167e
Arina <arina.milyutina.0005@gmail.com>
2024-07-17 22:46:02 +0300

    added method for find all books

--- src/main/java/com/example/project/controller/BookController.java
+++ src/main/java/com/example/project/controller/BookController.java
@@ -2,16 +2,17 @@ package com.example.project.controller;
 
 import com.example.project.dto.BookDto;
 import com.example.project.entity.Book;
+import com.example.project.exception.NotFoundException;
 import com.example.project.service.BookService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -24,5 +25,12 @@ public class BookController {
     public ResponseEntity<Book> addBook(@Valid @RequestBody BookDto bookDto) {
         Book newBook = bookService.createdBook(bookDto);
         return new ResponseEntity<>(newBook, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Book>> finAllBooks() throws NotFoundException {
+        List<Book> books = bookService.findAll();
+        return ResponseEntity.of(Optional.ofNullable(books));
     }
 }
