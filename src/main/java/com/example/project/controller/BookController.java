27b5ce0e6dd531f7c888571e9a3a274a512ca20c
Arina <arina.milyutina.0005@gmail.com>
2024-07-17 21:50:48 +0300

    created BookController, added method for process a POST request to add a new book

---
+++ src/main/java/com/example/project/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.example.project.controller;
+
+import com.example.project.dto.BookDto;
+import com.example.project.entity.Book;
+import com.example.project.service.BookService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+@Validated
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@Valid @RequestBody BookDto bookDto) {
+        Book newBook = bookService.createdBook(bookDto);
+        return new ResponseEntity<>(newBook, HttpStatus.CREATED);
+    }
+}
