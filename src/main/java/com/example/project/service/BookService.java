342521e0e75d6bc350582d8abd5b24ad2ecd6824
Arina <arina.milyutina.0005@gmail.com>
2024-07-18 01:03:27 +0300

    fixed method updateBook

--- src/main/java/com/example/project/service/BookService.java
+++ src/main/java/com/example/project/service/BookService.java
@@ -1,12 +1,16 @@
 package com.example.project.service;
 
+import com.example.project.dto.AuthorDto;
 import com.example.project.dto.BookDto;
+import com.example.project.dto.UpdateBookDto;
 import com.example.project.entity.Author;
 import com.example.project.entity.Book;
+import com.example.project.exception.AuthorNotFoundException;
 import com.example.project.exception.NotFoundException;
 import com.example.project.repository.AuthorRepository;
 import com.example.project.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -44,14 +48,22 @@ public class BookService {
     }
 
 
-    public Book updateBook(Long bookId, BookDto bookDto) throws NotFoundException {
-        Book book = bookRepository.findById(bookId).orElseThrow(() -> new NotFoundException("The book with is id was not found."));
+    public Book updateBook(Long bookId, UpdateBookDto bookDto) throws NotFoundException, AuthorNotFoundException {
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new NotFoundException("The book with this id was not found."));
+        AuthorDto authorDto = bookDto.getAuthorDto();
+        Author author = book.getAuthor();
+        author.setName(authorDto.getName());
+        author.setSurname(authorDto.getSurname());
+        author.setBirthDate(authorDto.getBirthDate());
+        authorRepository.save(author);
         book.setTitle(bookDto.getTitle());
         book.setGenre(bookDto.getGenre());
         book.setPublicationDate(bookDto.getPublicationDate());
-        book.setAuthor(bookDto.getAuthor());
+        book.setAuthor(author);
         return bookRepository.save(book);
     }
+
+}
 
 //    public void deleteBook(Long bookId) {
 //        Optional<Book> bookOptional = findBookById(bookId);
@@ -62,7 +74,8 @@ public class BookService {
 //        }
 //    }
 
-    public List<Book> findByAuthorId(Long authorId) {
-        return bookRepository.findByAuthorId(authorId);
-    }
-}
+//        public List<Book> findByAuthorId (Long authorId){
+//            return bookRepository.findByAuthorId(authorId);
+//        }
+
+
