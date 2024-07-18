461e471349919d1671c64f9bfc04c1a802c0dddb
Arina <arina.milyutina.0005@gmail.com>
2024-07-18 14:44:52 +0300

    realized method for update author

--- src/main/java/com/example/project/service/AuthorService.java
+++ src/main/java/com/example/project/service/AuthorService.java
@@ -1,9 +1,13 @@
 package com.example.project.service;
 
 import com.example.project.dto.AuthorDto;
+import com.example.project.dto.UpdateAuthorBookDto;
+import com.example.project.dto.UpdateAuthorDto;
 import com.example.project.entity.Author;
+import com.example.project.entity.Book;
 import com.example.project.exception.NotFoundException;
 import com.example.project.repository.AuthorRepository;
+import com.example.project.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,8 @@ import java.util.Optional;
 public class AuthorService {
     @Autowired
     private AuthorRepository authorRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
     public Author createdAuthor(AuthorDto authorDto) {
         Author author = Author.builder()
@@ -37,19 +43,32 @@ public class AuthorService {
         return Optional.ofNullable(authorRepository.findById(authorId).orElseThrow(() -> new NotFoundException("Author not found.")));
     }
 
-//    private void updateAuthor(Long authorId, Author updateAuthor) {
-//        Optional<Author> authorOptional = findById(authorId);
-//        if (authorOptional.isPresent()) {
-//            Author author = authorOptional.get();
-//            author.setName(updateAuthor.getName());
-//            author.setSurname(updateAuthor.getSurname());
-//            author.setBirthDate(updateAuthor.getBirthDate());
-//            author.setBooks(updateAuthor.getBooks());
-//            createdAuthor(author);
-//        } else {
-//            throw new NotFoundException("Book not found with id: " + authorId);
-//        }
-//    }
+    public Author updateAuthor(Long authorId, UpdateAuthorDto updateAuthorDto) throws NotFoundException {
+        Optional<Author> authorOptional = authorRepository.findById(authorId);
+        if (authorOptional.isPresent()) {
+            Author author = authorOptional.get();
+            author.setName(updateAuthorDto.getName());
+            author.setSurname(updateAuthorDto.getSurname());
+            author.setBirthDate(updateAuthorDto.getBirthDate());
+            author.getBooks().removeAll(author.getBooks());
+            if (updateAuthorDto.getBookList() != null) {
+                for (UpdateAuthorBookDto bookDto : updateAuthorDto.getBookList()) {
+                    Book book = Book.builder()
+                            .title(bookDto.getTitle())
+                            .genre(bookDto.getGenre())
+                            .publicationDate(bookDto.getPublicationDate())
+                            .author(author)
+                            .build();
+                    book = bookRepository.save(book);
+                    author.getBooks().add(book);
+                }
+            }
+            author = authorRepository.save(author);
+            return author;
+        } else {
+            throw new NotFoundException("Author not found with id: " + authorId);
+        }
+    }
 
     private void deleteAuthor(Long authorId) {
         authorRepository.deleteById(authorId);
